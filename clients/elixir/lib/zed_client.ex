def42219cdcedaedb2da3991e3ad43b56320049f
Alexander Mills <alexander.d.mills@gmail.com>
2026-08-09 17:15:54 -0500

    fix: restore Elixir and Swift validation gates

--- clients/elixir/lib/zed_client.ex
+++ clients/elixir/lib/zed_client.ex
@@ -1,5 +1,5 @@
 defmodule ZedClient do
   @enforce_keys [:base_url]
   defstruct [:base_url, :bearer_token]
-  def new(base_url, bearer_token \ nil), do: %__MODULE__{base_url: base_url, bearer_token: bearer_token}
+  def new(base_url, bearer_token \\ nil), do: %__MODULE__{base_url: base_url, bearer_token: bearer_token}
 end
